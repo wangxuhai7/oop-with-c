224467ee906e1f58545981524cf9f720597f3853
wangxuhai7 <wangxuhai7@yeah.net>
2022-07-18 00:40:18 +0800

    add the public ops with class
    v0.2

--- oopc.c
+++ oopc.c
@@ -22,10 +22,12 @@ typedef struct shape_func_ops_s {
 /* 方形继承形状这一子类 */
 typedef struct rectangle_s {
     shape_func_ops_t ops;
+    /* 私有成员 */
     int x;
     int y;
     int width;
     int height;
+    int (*change_width_height)(shape_func_ops_t *ops, int new_width, int new_height);
 } rectangle_t;
 
 /***************************************
@@ -44,9 +46,10 @@ typedef struct circle_s {
     int x;
     int y;
     int radius;
+    int (*change_radius)(shape_func_ops_t *ops, int radius);
 } circle_t;
 
-/* 形状这一类的多态：打印方形 */
+/* 形状这一类的公共类多态：打印方形 */
 int rectangle_draw(shape_func_ops_t *ops)
 {
     if (ops == NULL) {
@@ -58,7 +61,7 @@ int rectangle_draw(shape_func_ops_t *ops)
     return 0;
 }
 
-/* 形状这一类的多态：打印圆形 */
+/* 形状这一类的公共类多态：打印圆形 */
 int circle_draw(shape_func_ops_t *ops)
 {
     if (ops == NULL) {
@@ -67,6 +70,35 @@ int circle_draw(shape_func_ops_t *ops)
 
     circle_t *tmp = (circle_t *)ops;
     printf("circle draw: (x, y) = (%d, %d), radius = %d\n", tmp->x, tmp->y, tmp->radius);
+    return 0;
+}
+
+/* 方形的对外的public成员 */
+int change_width_height(shape_func_ops_t *ops, int new_width, int new_height)
+{
+    if (ops == NULL) {
+        return -1;
+    }
+
+    rectangle_t *tmp = (rectangle_t *)ops;
+    printf("change the width:height from [%d %d] ", tmp->width, tmp->height);
+    tmp->height = new_height;
+    tmp->width = new_width;
+    printf("to [%d %d] \n", tmp->width, tmp->height);
+    return 0;
+}
+
+/* 圆形的对外的public成员 */
+int change_radius(shape_func_ops_t *ops, int new_radius)
+{
+    if (ops == NULL) {
+        return -1;
+    }
+
+    circle_t *tmp = (circle_t *)ops;
+    printf("change the radius from %d ", tmp->radius);
+    tmp->radius = new_radius;
+    printf("to %d \n", tmp->radius);
     return 0;
 }
 
@@ -85,6 +117,7 @@ shape_func_ops_t *rectangle_new(int startx, int starty, int width_init, int heig
     tmp->ops.draw = rectangle_draw;
     tmp->ops.destory = NULL;
     tmp->ops.move = NULL;
+    tmp->change_width_height = change_width_height;
     return (shape_func_ops_t *)tmp;
 }
 
@@ -102,6 +135,7 @@ shape_func_ops_t *circle_new(int startx, int starty, int radius)
     tmp->ops.draw = circle_draw;
     tmp->ops.destory = NULL;
     tmp->ops.move = NULL;
+    tmp->change_radius = change_radius;
     return (shape_func_ops_t *)tmp;
 }
 
@@ -139,5 +173,14 @@ int main(int argc, char *argv[])
         assert(0);
     }    
 
+    /* 同时方形和圆形的x,y,width,height,radius都是private, 只有shape做为public */
+    /* 每个形状有自己的public */
+
+    if (change_radius(circle_obj, 40) != 0) {
+        assert(0);
+    }
+    if (change_width_height(rectangle_obj, 50, 60) != 0) {
+        assert(0);
+    }
     return 0;
 }
